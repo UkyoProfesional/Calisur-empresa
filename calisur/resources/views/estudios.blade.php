553b95e7ed08216c277fbef4b4a4c0d64bc91dba
Ukyo Moreno Cancino <146487624+UkyoProfesional@users.noreply.github.com>
2023-11-25 21:04:23 -0600

    Cambios minimos

--- calisur/resources/views/estudios.blade.php
+++ calisur/resources/views/estudios.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo')
-Calisur
+Estudios
 @endsection
 
 @section('contenido')
--- calisur/resources/views/productos.blade.php
+++ calisur/resources/views/productos.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('titulo')
-Calisur
+Productos
 @endsection
 
 @section('contenido')
