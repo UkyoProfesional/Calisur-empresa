4b21493b1231100f1cd964a434e0d6e561afbe19
ArICvF <arifcv17@gmail.com>
2023-12-14 11:50:58 -0600

    Update servicios.blade.php

--- calisur/resources/views/servicios.blade.php
+++ calisur/resources/views/servicios.blade.php
@@ -38,7 +38,7 @@ Servicios
         <div class="w-full px-5 mb-12 md:w-1/2 md:mb-2">
         <h2 class="mb-4 text-4xl font-bold text-[#360A13]">Mantenimiento y recarga de extintores</h2>
             <h3 class="mb-4 text-3xl font-bold text-[#360A13]">Revisión completa interna y externa:</br>Revision interna y externa:</h3>
-             <ul>
+             <ul class="list-disc">
                 <li>Prueba de funcionamiento de cilindro</li>
                 <li>Sustitución de partes</li>
                 <li>Reparaciones de partes</li>
@@ -46,7 +46,7 @@ Servicios
              </ul> 
             <br>
              <h3 class="mb-4 text-2xl font-semibold text-[#360A13]">Cada 12 meses: </h3>
-                <ul type="A">
+                <ul class="list-disc">
                     <li>Verificar el peso y presión del extintor</li>
                     <li>Comprobar el estado del agente del extintor.</li>
                     <li>Comprobar el estado de la manguera.</li>
@@ -54,9 +54,9 @@ Servicios
                 </ul>
                 <br>
                 <h3 class="mb-4 text-2xl font-semibold text-[#360A13]">Cada 5 años: </h3>
-                <ol type="A">
+                <ul class="list-disc" >
                     <li>Realización de una prueba hidráulica, que tendrá lugar 3 veces como máximo, ya que la vida útil de todo extintor es de 20 años</li>
-                </ol>
+                </ul>
                       
         </div>
         
@@ -85,16 +85,16 @@ Servicios
 
         <div class="w-full px-5 mb-12 md:w-1/2 md:mb-2">
         <h2 class="mb-4 text-4xl font-bold text-[#360A13]">REUBICACION Y MANTENIMIENTO DE BOMBAS CONTRA INCENDIO</h2>
-            <h3 class="mb-4 text-2xl underline text-[#360A13]">Ingenieria:</h3>           
-            <ul>
+            <h3 class="mb-4 text-2xl text-[#360A13]">Ingenieria:</h3>           
+            <ul class="list-disc">
                 <li>Cálculo hidráulico</li>
                 <li>Redistribución de tuberías</li>
                 <li>Protocolo de Izaje</li>
                 <li>Isométricos de obra, modelado 3D.</li>
             </ul>
             <br>
-            <h3 class="mb-4 text-2xl underline text-[#360A13]">Obra mecánica:</h3>           
-            <ul>
+            <h3 class="mb-4 text-2xl text-[#360A13]">Obra mecánica:</h3>           
+            <ul class="list-disc">
                 <li>Cálculo hidráulico</li>
                 <li>Redistribución de tuberías</li>
                 <li>Protocolo de Izaje</li>
@@ -102,15 +102,15 @@ Servicios
             </ul>
 
             <br>
-            <h3 class="mb-4 text-2xl underline text-[#360A13]">Obra eléctrica:</h3>           
-            <ul>
+            <h3 class="mb-4 text-2xl text-[#360A13]">Obra eléctrica:</h3>           
+            <ul class="list-disc">
                 <li>Desconexión y conexión de sistema eléctrico de red contra incendio.</li>
                 <li>Reubicación de tomas eléctricas y detectores de humo.</li>
             </ul>
 
             <br>
-            <h3 class="mb-4 text-2xl underline text-[#360A13]">Obra civil:</h3>           
-            <ul>
+            <h3 class="mb-4 text-2xl text-[#360A13]">Obra civil:</h3>           
+            <ul class="list-disc">
                 <li>Construcción de bases de concreto para motores</li>
                 <li>Construcción de caseta para resguardo de cabezal de pruebas.</li>
             </ul>
@@ -132,11 +132,11 @@ Servicios
 
         <div class="w-full px-5 mb-12 md:w-1/2 md:mb-2">
         <h2 class="mb-4 text-4xl font-bold text-[#360A13]">Transporte de residuos peligrosos</h2>
-            <ul>
-                <ol>Traslado de residuos peligrosos mediante</ol>
-                <ol>Translado de crudo</ol>
-                <ol>Disposición final de residuos peligrosos</ol>
-                <ol>Confinamiento de residuos peligrosos</ol>
+            <ul class="list-disc">
+                <li>Traslado de residuos peligrosos mediante</li>
+                <li>Translado de crudo</li>
+                <li>Disposición final de residuos peligrosos</li>
+                <li>Confinamiento de residuos peligrosos</li>
             </ul>
         </div>
     </div>
