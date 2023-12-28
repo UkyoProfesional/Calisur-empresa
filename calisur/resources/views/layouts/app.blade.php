1f93869c2ebd14bb90e1f40c502696c5d37473e6
Ukyo Moreno Cancino <146487624+UkyoProfesional@users.noreply.github.com>
2023-12-28 02:31:40 -0600

    Mejora en la navegacion
    Se mejoro la navegación y se agrego mas imágenes

--- calisur/resources/views/layouts/app.blade.php
+++ calisur/resources/views/layouts/app.blade.php
@@ -15,18 +15,18 @@
 <body>
     <header class="p-5 bg-[#ebe4e5] border-b shadow">
         <div class="container flex items-center justify-between mx-auto">
-            <a href="/">
+            <a href="{{ route('principal')}}">
                 <img src="/img/logo.png" alt="Logo de la empresa" class="h-20 m-4 w-35 ">
             </a>
             <nav class="flex items-center gap-11">
-                <a href="/estudios"
-                    class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Estudios</a>
-                <a href="/servicios"
-                    class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Servicios</a>
-                <a href="/productos"
-                    class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Productos</a>
+                <a href="{{ route('estudios.index') }}"
+                    class="text-sm font-bold  uppercase  {{request()->routeIs('estudios.index') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Estudios</a>
+                <a href="{{ route('servicios.index') }}"
+                    class="text-sm font-bold uppercase  {{request()->routeIs('servicios.index') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Servicios</a>
+                <a href="{{ route('productos.index') }}"
+                    class="text-sm font-bold uppercase {{request()->routeIs('productos.index') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Productos</a>
                 <a
-                    href="/contacto"class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Contacto</a>
+                    href="{{ route('contacto.index') }}" class="text-sm font-bold uppercase {{request()->routeIs('contacto.index') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Contacto</a>
 
             </nav>
             <nav class="flex items-center gap-3">
@@ -43,7 +43,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                                 <button type="submit"
-                                    class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">
+                                    class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee] ">
                                     Cerrar Sesion
                                 </button>
                             </form>
@@ -54,9 +54,9 @@
                     @guest
                         <nav class="flex items-center gap-5">
                             <a href="{{ route('login') }}"
-                                class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Login</a>
+                                class="text-sm font-bold  uppercase {{request()->routeIs('login') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Login</a>
                             <a href="{{ route('register') }}"
-                                class="text-sm font-bold text-[#360A13] uppercase hover:text-[#793341ee]">Crear Cuenta</a>
+                                class="text-sm font-bold uppercase {{request()->routeIs('register') ? 'text-[#360A13] hover:text-[#793341ee]' : 'text-[#793341ee] hover:text-[#360A13]'}}">Crear Cuenta</a>
 
                         </nav>
 
--- calisur/resources/views/productos.blade.php
+++ calisur/resources/views/productos.blade.php
@@ -21,7 +21,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/sis_espuma.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 font-semibold text-center text-[#360A13]">Sistemas de Espuma</p>
                     <p class="p-4 text-center text-[#360A13]">Equipos especializados que aplican espuma retardante para combatir incendios con eficacia.</p>
@@ -30,7 +30,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/tambo_espuma.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Tambores de Espuma</p>
                     <p class="p-4 text-center text-[#360A13]">Recipientes diseñados para almacenar y dispensar espuma contra incendios de manera segura.</p>
@@ -39,7 +39,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/espuma_incendio.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Espuma Contra Incendios</p>
                     <p class="p-4 text-center text-[#360A13]">Producto químico esencial para extinguir incendios al crear una capa de espuma que sofoca las llamas.</p>
@@ -48,7 +48,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/boquilla_monitor.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Boquillas de Monitores</p>
                     <p class="p-4 text-center text-[#360A13]">Dispositivos de dirección de agua que permiten una extinción precisa del fuego.</p>
@@ -57,7 +57,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/valvula_monitor.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Válvula de Compuerta para Monitores</p>
                     <p class="p-4 text-center text-[#360A13]">Válvula especializada para controlar y dirigir el flujo de agua en sistemas de monitores contra incendios.</p>
@@ -66,7 +66,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/valvulalib_aire.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Válvulas Liberadoras de Aire</p>
                     <p class="p-4 text-center text-[#360A13]">Válvulas diseñadas para liberar aire en sistemas hidráulicos, evitando cavitación y garantizando un rendimiento óptimo.</p>
@@ -75,7 +75,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/valvula_alivio_presion.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Válvulas de Seguridad de Alivio de Presión</p>
                     <p class="p-4 text-center text-[#360A13]">Dispositivos de seguridad que liberan automáticamente la presión excesiva para prevenir fallos en el sistema.</p>
@@ -84,7 +84,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/valvula_globo.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Válvulas de Compuerta, Bola y Globo</p>
                     <p class="p-4 text-center text-[#360A13]">Diversos tipos de válvulas para regular el flujo de líquidos y gases en sistemas contra incendios.</p>
@@ -100,7 +100,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/valvula_guillotina.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 font-semibold text-center text-[#360A13]">Válvulas de Guillotina y Mariposa</p>
                     <p class="p-4 text-center text-[#360A13]">Válvulas especializadas para controlar y detener el flujo de líquidos o gases.</p>
@@ -109,7 +109,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/extintor.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Extintores</p>
                     <p class="p-4 text-center text-[#360A13]">Dispositivos portátiles y efectivos para extinguir pequeños incendios en su fase inicial.</p>
@@ -118,7 +118,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/manguera_incendio.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Mangueras Contra Incendio</p>
                     <p class="p-4 text-center text-[#360A13]">Equipos esenciales para la distribución controlada de agua y otros agentes extintores.</p>
@@ -127,7 +127,7 @@ Productos
                 <div class="w-2/5 mx-auto mb-4 overflow-hidden bg-[#ebe4e5] rounded-lg">
                     <!-- Contenido del contenedor -->
                     <div class="flex items-center justify-center">
-                        <img src="/img/descargar.png" alt="Imagen" class="w-2/5 pt-5 rounded">
+                        <img src="/img/detector_humo.png" alt="Imagen" class="w-2/5 pt-5 rounded">
                     </div>
                     <p class="pt-4 text-center font-semibold text-[#360A13]">Alarmas de Detección de Humo y Gas</p>
                     <p class="p-4 text-center text-[#360A13]">Sistemas de alerta temprana que detectan la presencia de humo o gases peligrosos para una rápida evacuación.</p>
